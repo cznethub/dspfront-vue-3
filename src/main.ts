9060b8e6d83a81b5ac80aca7716f1a689eefa157
Devin Cowan <dcowan@cuahsi.org>
2023-05-17 16:45:19 -0400

    more pinia

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import '@jsonforms/vue-vanilla/vanilla.css';
 import '@/assets/css/global.scss'
 
-import Vuex from 'vuex'
-import { Model } from 'pinia-orm'
-import createPersistedState from 'vuex-persistedstate'
+import { createPinia } from 'pinia'
+import { Model, createORM } from 'pinia-orm'
+// import createPersistedState from 'vuex-persistedstate'
 import VueCookies from 'vue-cookies'
 import Buefy from '@dword-design/buefy'
 import VueRouter from 'vue-router'
 import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import vueFilterPrettyBytes from 'vue-filter-pretty-bytes'
 import vuetify from '@/plugins/vuetify'
@@ -15,9 +16,9 @@ import browserDetect from "vue-browser-detect-plugin"
 import IdleVue from 'idle-vue'
 
 import { router } from './router'
-import { orm } from '@/models/orm'
-import { persistedPaths } from './models/persistedPaths'
-import { APP_NAME } from './constants'
+// import { orm } from '@/models/orm'
+// import { persistedPaths } from './models/persistedPaths'
+// import { APP_NAME } from './constants'
 
 // Uncomment to filter out errors
 // Vue.config.errorHandler = (err, vm, info) => {
@@ -28,29 +29,31 @@ import { APP_NAME } from './constants'
       //     }
       //   }
       // }
-      
-Vue.config.productionTip = false
-Vue.use(Vuex)
+const app = createApp(App)
+// app.config.productionTip = false
+
+const pinia = createPinia().use(createORM())
+app.use(pinia)
 
 // Create Vuex Store and register database through Vuex ORM.
-const store = new Vuex.Store({
-  plugins: [
-    VuexORM.install(orm),
-    createPersistedState({
-      paths: persistedPaths,
-      key: APP_NAME
-    })
-  ]
-})
+// const store = new Vuex.Store({
+//   plugins: [
+//     VuexORM.install(orm),
+//     createPersistedState({
+//       paths: persistedPaths,
+//       key: APP_NAME
+//     })
+//   ]
+// })
 
 const eventsHub = new Vue()
       
-Vue.use(vueFilterPrettyBytes)
-Vue.use(VueRouter)
-Vue.use(VueCookies)
-Vue.use(browserDetect);
-Vue.use(IdleVue, { eventEmitter: eventsHub, idleTime: 60000, store })
-Vue.use(Buefy, {
+app.use(vueFilterPrettyBytes)
+app.use(VueRouter)
+app.use(VueCookies)
+app.use(browserDetect);
+app.use(IdleVue, { eventEmitter: eventsHub, idleTime: 60000, store })
+app.use(Buefy, {
   defaultIconPack: 'fas',
   defaultContainerElement: '#content',
   defaultNotificationPosition: 'is-top',
@@ -60,7 +63,7 @@ Vue.use(Buefy, {
 
 new Vue({
   router,
-  store,
+  // store,
   vuetify,
   render: (h) => h(App),
 }).$mount('#app')
