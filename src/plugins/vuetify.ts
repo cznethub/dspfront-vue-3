4289511b324fd1d2b5c260d0ff114e4f84574f2e
Devin Cowan <dcowan@cuahsi.org>
2023-05-17 19:43:25 -0400

    vuetify only load used components

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,11 +1,4 @@
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
-const vuetify = createVuetify({
-  components,
-  directives,
-})
-
-export default vuetify
+export default createVuetify()
